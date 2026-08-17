a50002a95c0f38c9ace11f5bd616271895044620
sillypointsiva <raagavansiva@zohomail.in>
2026-08-17 13:39:16 +1200

    Add gpx to workbox globPatterns

--- vite.config.js
+++ vite.config.js
@@ -19,7 +19,7 @@ export default defineConfig({
           { src: 'icon-512-maskable.png', sizes: '512x512', type: 'image/png', purpose: 'maskable' }
         ]
       },
-      workbox: { globPatterns: ['**/*.{js,css,html,svg,png}'] }
+      workbox: { globPatterns: ['**/*.{js,css,html,svg,png,gpx}'] }
     })
   ]
 })
